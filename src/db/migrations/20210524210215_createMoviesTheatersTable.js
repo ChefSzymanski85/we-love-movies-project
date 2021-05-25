d273a6cbeefa33049df8bf7ccbb9214d3c993606
Mark Szymanski <chefszymanski85@gmail.com>
2021-05-24 21:31:56 -0400

    re-ordered migration files

--- src/db/migrations/20210524210215_createMoviesTheatersTable.js
+++ src/db/migrations/20210524210215_createMoviesTheatersTable.js
@@ -1,11 +1,17 @@
 exports.up = function (knex) {
   return knex.schema.createTable("movies_theaters", (table) => {
     table.integer("movie_id").unsigned().notNullable();
-    table.foreign("movie_id").references("movie_id").inTable("movies");
-    //.onDelete("CASCADE");
+    table
+      .foreign("movie_id")
+      .references("movie_id")
+      .inTable("movies")
+      .onDelete("CASCADE");
     table.integer("theater_id").unsigned().notNullable();
-    table.foreign("theater_id").references("theater_id").inTable("theaters");
-    //.onDelete("CASCADE");
+    table
+      .foreign("theater_id")
+      .references("theater_id")
+      .inTable("theaters")
+      .onDelete("CASCADE");
     table.bool("is_showing");
   });
 };
